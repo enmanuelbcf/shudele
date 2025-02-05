364ab838a724d6dca90e13fdeb2947d7b434c63d
enmanuel <enmanuelbcf2@gmail.com>
2025-02-04 21:01:52 -0400

    incluir: protecion de los endpoint atravez de JWT 1/2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import sqlite3
 from logging import raiseExceptions
+from wsgiref.util import request_uri
 
 import uvicorn
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Form
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from typing import Annotated
+from jose import jwt
 from pydantic import BaseModel, ValidationError
+from sniffio import AsyncLibraryNotFoundError
 from starlette.status import HTTP_400_BAD_REQUEST
 
 
 # Conectar a la base de datos
 import sqlite3
 
-# Conectar a la base de datos
+
 def conectar_db():
     try:
         con = sqlite3.connect('./DataBases/db')
@@ -64,36 +70,75 @@ def get_one(id):
         return None
 
 app = FastAPI()
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+def encode_token(pyload: dict)->str:
+    token = jwt.encode(pyload,'my-secret', algorithm='HS256')
+    return token
+
+def decode_token(token: Annotated[str, Depends(oauth2_schema)])-> dict:
+    data = jwt.decode(token, 'my-secret', algorithms=['HS256'])
+    user = users.get(data['username'])
+    return user
+
+users = {
+    'enmanuelbcf':{
+        'username':'Enmanuel',
+        'email': 'enmanuelbcf@gmail.com',
+        'password': 'Prueba01*'
+    }
+}
+@app.post('/token')
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = users.get(form_data.username)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='No se encontro el usuario')
+    token = encode_token({'username': user['username'], 'email': user['email']})
+    return {'access_token': token}
 
 @app.get("/universidad/obtener-universidades")
-def obtener_universidades():
+def obtener_universidades(my_user: Annotated[dict, Depends(decode_token)]):
     try:
         data = get_all()
 
         if not data:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron datos")
 
+
         return {"status": "success", "data":data}
 
     except HTTPException as err:
+        if err.status_code == status.HTTP_401_UNAUTHORIZED:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Endpoint protegido por token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         raise err
     except Exception as a:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Error Interno')
 
 @app.get("/universidad/obtener-universidad/{id}")
-def obtener_universid(id:int):
+def obtener_universid(id: int, my_user: Annotated[dict, Depends(decode_token)]):  # Protegido con token
     try:
         data = get_one(id)
 
         if not data:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron datos")
 
-        return {"status": "success", "data":data}
+        return {"status": "success", "data": data}
 
     except HTTPException as err:
+        if err.status_code == status.HTTP_401_UNAUTHORIZED:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Endpoint protegido por token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         raise err
-    except Exception as a:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Error Interno')
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error Interno")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
