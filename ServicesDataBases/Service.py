2247dbcb7033b1f60b6307a9463f66d0c5ddcfc7
enmanuel <enmanuelbcf2@gmail.com>
2025-02-15 18:33:35 -0400

    fueture/separar servicios de la db

--- ServicesDataBases/Service.py
+++ ServicesDataBases/Service.py
@@ -1,19 +1,92 @@
 import sqlite3
+from Model.university_model import University
+
 
 def conectar_db():
     try:
-        con = sqlite3.connect('../DataBases/dbshedule')
+        con = sqlite3.connect('./DataBases/db')
+        con.row_factory = sqlite3.Row  # Permite obtener los resultados como diccionarios
         return con
+    except sqlite3.Error as e:
+        print(f'Error al conectar: {e}')
+        return None
+
+def create_universidad(universidad: University ):
+    con = conectar_db()
+    if con is None:
+        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
+
+    try:
+        cursor = con.cursor()
+        datos_insertados = cursor.execute("INSERT into Universidad values(?,?,?,?)",
+                                          (
+            universidad.universidad_id,
+            universidad.nombre_universidad,
+            universidad.acronimo_universidad,
+            universidad.foto
+        ))
+
+
+        if datos_insertados.rowcount > 0:
+            con.commit()
+            return {'status': 'success',
+                    'data': {
+                        'universidad_id':universidad.universidad_id,
+                        'nombre_universidad':universidad.nombre_universidad,
+                        'acronimo_universidad': universidad.acronimo_universidad,
+                        'foto': universidad.foto
+                        }
+                    }
+        return  None
 
     except sqlite3.Error as e:
-        print(f'{e.sqlite_errorname}-{e.sqlite_errorcode}')
+        print(f'Error en la consulta: {e}')
+        return None
 
-def get_all():
+
+
+
+
+# Obtener todas las universidades y devolver un objeto JSON clave-valor
+def get_all_universidades():
     con = conectar_db()
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM Universidad")
-    data = cursor.fetchall()
-    con.close()
-    return data
+    if con is None:
+        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
 
+    try:
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM Universidad")
+        rows = cursor.fetchall()
+        con.close()  # Cerrar conexión
+
+        if rows:
+            data = [dict(row) for row in rows]
+            return data
+        else:
+            return None
+
+    except sqlite3.Error as e:
+        print(f'Error en la consulta: {e}')
+        return None
+
+def get_one_universidad(id):
+    con = conectar_db()
+    if con is None:
+        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
+
+    try:
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM Universidad where universidad_id = ? ",[id] )
+        rows = cursor.fetchall()
+        con.close()  # Cerrar conexión
+
+        if rows:
+            data = [dict(row) for row in rows]  # Convertir cada fila en un diccionario clave-valor
+            return data
+        else:
+            return None
+
+    except sqlite3.Error as e:
+        print(f'Error en la consulta: {e}')
+        return None
 
--- main.py
+++ main.py
@@ -20,98 +20,10 @@ from pydantic import BaseModel, ValidationError
 from sniffio import AsyncLibraryNotFoundError
 from starlette.status import HTTP_400_BAD_REQUEST
 
+from ServicesDataBases.Service import get_all_universidades, get_one_universidad, create_universidad
 
 # Conectar a la base de datos
-import sqlite3
 
-
-def conectar_db():
-    try:
-        con = sqlite3.connect('./DataBases/db')
-        con.row_factory = sqlite3.Row  # Permite obtener los resultados como diccionarios
-        return con
-    except sqlite3.Error as e:
-        print(f'Error al conectar: {e}')
-        return None
-
-def create_universidad(universidad: University ):
-    con = conectar_db()
-    if con is None:
-        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
-
-    try:
-        cursor = con.cursor()
-        datos_insertados = cursor.execute("INSERT into Universidad values(?,?,?,?)",
-                                          (
-            universidad.universidad_id,
-            universidad.nombre_universidad,
-            universidad.acronimo_universidad,
-            universidad.foto
-        ))
-
-
-        if datos_insertados.rowcount > 0:
-            con.commit()
-            return {'status': 'success',
-                    'data': {
-                        'universidad_id':universidad.universidad_id,
-                        'nombre_universidad':universidad.nombre_universidad,
-                        'acronimo_universidad': universidad.acronimo_universidad,
-                        'foto': universidad.foto
-                        }
-                    }
-        return  None
-
-    except sqlite3.Error as e:
-        print(f'Error en la consulta: {e}')
-        return None
-
-
-
-
-
-# Obtener todas las universidades y devolver un objeto JSON clave-valor
-def get_all_universidades():
-    con = conectar_db()
-    if con is None:
-        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
-
-    try:
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM Universidad")
-        rows = cursor.fetchall()
-        con.close()  # Cerrar conexión
-
-        if rows:
-            data = [dict(row) for row in rows]
-            return data
-        else:
-            return None
-
-    except sqlite3.Error as e:
-        print(f'Error en la consulta: {e}')
-        return None
-
-def get_one_universidad(id):
-    con = conectar_db()
-    if con is None:
-        return {"status": "error", "message": "Error al conectar a la base de datos", "data": []}
-
-    try:
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM Universidad where universidad_id = ? ",[id] )
-        rows = cursor.fetchall()
-        con.close()  # Cerrar conexión
-
-        if rows:
-            data = [dict(row) for row in rows]  # Convertir cada fila en un diccionario clave-valor
-            return data
-        else:
-            return None
-
-    except sqlite3.Error as e:
-        print(f'Error en la consulta: {e}')
-        return None
 
 app = FastAPI()
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='obtener-token')
