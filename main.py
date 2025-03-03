14e63259a2aae60401865205fb5428d75316cc32
enmanuel <enmanuelbcf2@gmail.com>
2025-03-03 00:37:19 -0400

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import asyncio
+from datetime import datetime
+
+import pytz
 import uvicorn
 from fastapi import FastAPI
 from routers import usuario, universidades, auth
@@ -6,8 +9,12 @@ from routers.push_notification import send_push
 from contextlib import asynccontextmanager
 
 app = FastAPI()
+zona_horaria = pytz.timezone('America/Santo_Domingo')  # Zona horaria de RD, que es UTC -4
 
-# send_push()
+# Obtener la hora actual en la zona horaria de UTC -4
+ahora = datetime.now(zona_horaria)
+hora_actual = ahora.time()
+print(hora_actual)
 app.include_router(auth.router)
 app.include_router(usuario.router)
 app.include_router(universidades.router)
